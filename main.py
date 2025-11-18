33c9a76b6bde909229023fdf6e4a7830834204dc
FatmaDhouib <dhouib1995fatma@gmail.com>
2025-11-18 13:29:13 +0100

    Update access_netmiko to save command output
    Refactor access_netmiko function to write output to a file.

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-
 from netmiko import ConnectHandler
+
 cisco_router = {
 	'device_type' : 'cisco_ios',
-	'host' : ' 'sandbox-iosxr-1.cisco.com',
+	'host' : 'sandbox-iosxr-1.cisco.com',
 	'username' : 'admin',
 	'password' : 'C1sco12345',
 	'port' : 22,
 }
+
 ssh = ConnectHandler(**cisco_router)
 
-def acces_netmiko()
+def acces_netmiko():
 
-	result1 = ssh.send_command('show clock')
+	output1 = ssh.send_command('show clock')
 
-	result2 = ssh.send_command('show ip int br')> interfaces.txt
+	output = ssh.send_command('show ip int br')
+
+	with open ("interfaces.txt", "w") as f:
+
+		f.write(output)
+	print("interfaces to interfaces.txt")
+
+acces_netmiko()
 
 
 def dire_salut():
+
 	print("Salut, Git!")
 
 
